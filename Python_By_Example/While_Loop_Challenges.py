85c7e3afc4aa3d35dc5cb68ea5d7603431724010
Kocatas <kocatasefe@gmail.com>
2022-05-14 15:44:53 +0300

    Updated Files

---
+++ Python_By_Example/While_Loop_Challenges.py
@@ -0,0 +1,10 @@
+# Explanation of While Loop
+
+print("Till the total is equal 100,")
+total = 0
+while total < 100:
+    num = int(input("Enter a number: "))
+    total = total + num
+print("The total is", total)
+
+
