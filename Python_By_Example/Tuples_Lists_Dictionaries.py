6b90efe05e926b75d26598562476f7ea2d8e982b
Kocatas <kocatasefe@gmail.com>
2022-05-17 13:25:14 +0300

    Updated Files

--- Python_By_Example/Tuples_Lists_Dictionaries.py
+++ Python_By_Example/Tuples_Lists_Dictionaries.py
@@ -5,4 +5,17 @@
 # List include different data types at the same type. But it is not recommended to use due to unwanted results.
 # Arrays are used to store ONLY numbers in Python. In the other programming languages array corresponds
 # -the Lists in Python.
+# Dictionaries - the content of a dictionary can also be changed while the program is running.
+# index of dictionary is not changed if other rows of data are added or deleted
+# -while program is running unlike LIST.
 
+# Tuple Declaration:
+fruit_tuple = ("Apple", "Banana", "strawberry", "Orange")
+print(fruit_tuple.index("Apple"))
+print(fruit_tuple.index("Banana"))
+print(fruit_tuple.index("strawberry"))
+print(fruit_tuple[0])
+print(fruit_tuple[1])
+print(fruit_tuple[2])
+
+# List Declaration:
