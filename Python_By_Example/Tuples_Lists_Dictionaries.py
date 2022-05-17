ffe9c43f7c8cd3fa3ba17b67198d7c678f8aa01c
Kocatas <kocatasefe@gmail.com>
2022-05-17 09:57:22 +0300

    Updated Files

---
+++ Python_By_Example/Tuples_Lists_Dictionaries.py
@@ -0,0 +1,8 @@
+# Once a tuple is defined you cannot change what is stored in it.
+# Tuples are usually for menu items that would not need to be changed.
+# The contents of a List can be changed while the program is running and lists are one of the most common ways
+# - to store a collection of data under one variable name in Python.
+# List include different data types at the same type. But it is not recommended to use due to unwanted results.
+# Arrays are used to store ONLY numbers in Python. In the other programming languages array corresponds
+# -the Lists in Python.
+
