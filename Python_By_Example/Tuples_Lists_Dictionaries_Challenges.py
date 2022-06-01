e2dc4f6f1d18fd4b7771f539c2bd755bf0de6aa8
Kocatas <kocatasefe@gmail.com>
2022-06-01 23:46:59 +0300

    Updated Files

---
+++ Python_By_Example/Tuples_Lists_Dictionaries_Challenges.py
@@ -0,0 +1,7 @@
+# Challenge 069
+# Creating a tuple that is containing the names of five countries
+
+country_tuple = ("The United States", "Germany", "Belgium", "The Netherlands", "Spain")
+print(country_tuple[0:5])
+country = (input("Please enter one of the countries:"))
+print()
