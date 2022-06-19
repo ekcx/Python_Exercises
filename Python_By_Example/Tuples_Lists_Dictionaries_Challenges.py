9030f4af4bf17bcff06ad4516efe2c294c48976b
Kocatas <kocatasefe@gmail.com>
2022-06-19 15:41:52 +0300

    Updated Files

--- Python_By_Example/Tuples_Lists_Dictionaries_Challenges.py
+++ Python_By_Example/Tuples_Lists_Dictionaries_Challenges.py
@@ -1,12 +1,15 @@
 # Challenge 069
-# Creating a tuple that is containing the names of five countries
-
+# Creating a tuple that is containing the names of five countries.
+# Tuples are constant when the program is running.
+# Creating Tuples
 country_tuple = ("The United States", "Germany", "Belgium", "The Netherlands", "Spain")
 print(country_tuple[0:5])
+print(len(country_tuple))
 
-print("This is the times table: ")
-for i in range(1,10):
-    for j in range(1,10):
-        print(f"{i} * {j} = {i*j}")
-    print("\n")
+my_string = input("Please enter the country: ")
 
+# The For loop that is searching entered country in the tuple positions, then display it.
+
+for i in range(0, 5):
+    if my_string == country_tuple[i]:
+        print(f"The index of the country is {i}")
