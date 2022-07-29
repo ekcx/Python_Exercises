98f2e390aa5d7c65af3662c6a5f3ddcdcc84c82d
Efe Kocataş <kocatasefe@gmail.com>
2022-07-29 13:54:01 +0300

    Updated Files

---
+++ Python_Books/More_String_Manipulation.py
@@ -0,0 +1,21 @@
+# More String Manipulation #
+
+msg = "My First Message"
+msg_second = "MY SECOND MESSAGE"
+msg_third = "my third message"
+
+if msg.isupper():
+    print("UpperCase")
+else:
+    print("It is not upperCase")
+
+if msg_second.isupper():
+    print("UpperCase")
+else:
+    print("It is not upperCase")
+
+if msg_third.islower():
+    print("LowerCase")
+else:
+    print("It is not LowerCase")
+
