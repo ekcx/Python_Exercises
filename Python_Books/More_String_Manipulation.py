8f09ae93f0484eae37b6fd4267ff386bd35822e8
Efe Kocataş <kocatasefe@gmail.com>
2022-07-29 16:20:06 +0300

    Updated Files

--- Python_Books/More_String_Manipulation.py
+++ Python_Books/More_String_Manipulation.py
@@ -44,6 +44,8 @@ course = input("Type a course that you like: ")
 for i in course:
     print(i, end="-")
 
+# Challenge 082
+
 my_str = "Much Ado About Nothing"
 print("Much Ado About Nothing")
 starting = int(input("Please type starting point: "))
@@ -52,9 +54,30 @@ ending = int(input("Please type ending point: "))
 for i in range(starting, ending+1):
     print(my_str[i])
 
+# Challenge 083
 
 upper_case = input("Type in a upper case string: ")
 
 while not upper_case.isupper():
     print("The string is not in the uppercase form! You will write again the string untill to become uppercase idiot!")
     upper_case = input("Type in a upper case string: ")
+
+# Challenge 084
+
+post_code = input("Type your post code: ")
+
+for i in range(0, 2):
+    print(post_code[i].upper())
+
+# Challenge 085
+
+count = 0
+
+for i in range(0, len(name)):
+    if name[i].lower() in ('a', 'e', 'i', 'o', 'u'):
+        count += 1
+    else:
+        pass
+print("The total vowel numbers: %d " % count)
+
+
