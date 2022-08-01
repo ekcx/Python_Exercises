10b411ffed02c2143224a5ff8a221e4ee1286b8f
Efe Kocataş <kocatasefe@gmail.com>
2022-08-01 10:32:11 +0300

    Updated Files

--- Python_Books/Numeric_Arrays.py
+++ Python_Books/Numeric_Arrays.py
@@ -13,3 +13,31 @@ nums.append(newValue)
 nums.reverse()
 for i in nums:
     print(i)
+
+# This method is sorting in ascending order.
+
+nums = sorted(nums)
+print(nums)
+
+nums.pop()
+print(nums)
+
+# declaring an integer array #
+my_array = array('i', [])
+
+# specifying the size of the list #
+length = int(input("Type the limit: "))
+
+# adding the numbers to the nums until to reach the size #
+for i in range(0, length):
+    number = int(input("Type a number that will be adding to the list: "))
+    my_array.append(number)
+nums.extend(my_array)
+print(nums)
+
+# in here, we are typing the number itself to REMOVE. Not index!!!! #
+GetRid = int(input("Type the number of will be removed element: "))
+nums.remove(GetRid)
+print(nums)
+
+print(nums.count(45))
