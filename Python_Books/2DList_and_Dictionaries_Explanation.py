b9bc9c4536eb71172a5006a38261f38c11511029
Efe Kocataş <kocatasefe@gmail.com>
2022-08-02 11:30:02 +0300

    Explanation of 2D Lists and dictionaries.

---
+++ Python_Books/2DList_and_Dictionaries_Explanation.py
@@ -0,0 +1,14 @@
+# Two-Dimensional List #
+# It's like a two-dimensional array #
+
+grades_one = [[45, 37, 54], [62, 58, 59],
+              [49, 47, 60], [78, 83, 62]]
+
+# Dictionary #
+
+grades = [{"Ma":45, "En":37, "Fr":54},
+          {"Ma": 62,"En": 58,"Fr": 59},
+          {"Ma":78, "En":47, "Fr":60}]
+
+print(grades_one[0][1])
+print(grades[0]["En"])
