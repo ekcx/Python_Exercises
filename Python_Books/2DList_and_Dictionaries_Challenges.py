b0a149fcc99172f0b92d1230452fc7a121a52654
Efe Kocataş <kocatasefe@gmail.com>
2022-08-02 16:38:45 +0300

    Challenge 099

--- Python_Books/2DList_and_Dictionaries_Challenges.py
+++ Python_Books/2DList_and_Dictionaries_Challenges.py
@@ -10,7 +10,22 @@ column = int(input("Type column number: "))
 print(simple_2D_list[row][column])
 
 # Challenge 098 #
-display = int(input("Type a displayed row number: "))
-print(simple_2D_list[display])
-simple_2D_list[display].append(display)
+display_row = int(input("Type a displayed row number: "))
+print(simple_2D_list[display_row])
+simple_2D_list[display_row].append(display_row)
 print(simple_2D_list)
+
+# Challenge 099 #
+display_column = int(input("Type a displayed column number: "))
+#print(simple_2D_list[display_row][display_column])
+
+yes_or_no = input(f"Will be deleted [{display_row}]x[{display_column}]? y/n ?")
+if yes_or_no == "y":
+    new_data = int(input("Type a new number: "))
+    simple_2D_list[display_row][display_column] = new_data
+else:
+    pass
+
+print()
+print(simple_2D_list)
+
