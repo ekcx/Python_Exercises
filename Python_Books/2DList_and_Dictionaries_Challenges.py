bcae9f2904a33f7246992dc0f071b93b086f23f2
Efe Kocataş <kocatasefe@gmail.com>
2022-08-03 15:00:25 +0300

    Challenge 100 - 101

--- Python_Books/2DList_and_Dictionaries_Challenges.py
+++ Python_Books/2DList_and_Dictionaries_Challenges.py
@@ -11,13 +11,15 @@ print(simple_2D_list[row][column])
 
 # Challenge 098 #
 display_row = int(input("Type a displayed row number: "))
-print(simple_2D_list[display_row])
+
+# print(simple_2D_list[display_row])
 simple_2D_list[display_row].append(display_row)
-print(simple_2D_list)
+
+# print(simple_2D_list)
 
 # Challenge 099 #
 display_column = int(input("Type a displayed column number: "))
-#print(simple_2D_list[display_row][display_column])
+# print(simple_2D_list[display_row][display_column])
 
 yes_or_no = input(f"Will be deleted [{display_row}]x[{display_column}]? y/n ?")
 if yes_or_no == "y":
@@ -27,5 +29,21 @@ else:
     pass
 
 print()
-print(simple_2D_list)
+print(simple_2D_list[display_row])
+
+# Challenge 100 #
+
+sales_person = {"John" : {"N": 3056, "S": 8463, "E": 8441, "W": 2694},
+                "Tom"  : {"N": 4832, "S": 6786, "E": 4737, "W": 3612},
+                "Anne" : {"N": 5239, "S": 4802, "E": 5820, "W": 1859},
+                "Fiona": {"N": 3904, "S": 3645, "E": 8821, "W": 2451}}
+
+print(sales_person["John"])
+
+# Challenge 101
+
+user_name = input("Type user name: ")
+region_name = input("Type region name: ")
+print(sales_person[user_name][region_name])
+print(sales_person[user_name])
 
