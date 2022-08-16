cabd0fdda544c136259cb0aebfe8806c8af8304b
Efe Kocataş <kocatasefe@gmail.com>
2022-08-16 15:01:54 +0300

    CSV Files Details

--- Python_Books/csv_files_summary.py
+++ Python_Books/csv_files_summary.py
@@ -1,25 +1,30 @@
 import csv
 
 file = open("csv_files_summary.csv", "w")
-new_record = "Kocatas, 73, Gemini\n"
+new_record = "Brian, 73, Gemini\n"
 file.write(new_record)
 file.close()
 
+file = open("csv_files_summary.csv", "r")
 
+for row in file:
+    print(row)
+
+file.close()
 for i in range(0, 4):
     file = open("csv_files_summary.csv", "a")
     new_record = input("Type your name, birth year and your sign of zodiac: ")
-    file.write("\n")
-    file.write(new_record)
+    file.write(new_record + "\n")
 
 file.close()
 file = open("csv_files_summary.csv", "r")
 
-for i in file:
-    print(file.read())
-    print("\n")
+for row in file:
+    print(row)
 
 file.close()
 
 # the sense of the csv write/read/append process; first define the operation, second do the operation, then close the
-# connection.
+# files and operation.
+
+
